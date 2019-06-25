fbfdc1bfbd328c55d732b4b31754a3c43cc83a43
Sherif Elfadaly <sh.elfadaly@gmail.com>
2019-06-25 14:35:44 +0200

    validation rules enhancments

--- app/modules/users/user-routes.js
+++ app/modules/users/user-routes.js
@@ -3,7 +3,7 @@ const userController = container.userController;
 module.exports = (router) => {
   router.get('/', userController.all);
   router.get('/:id', userController.find);
-  router.post('/', container.validator.body(container.userValidationRules.insert), userController.insert);
+  router.post('/', container.userValidationRules('insert'), userController.insert);
 
   return router;
 };
--- app/modules/users/user-validation-rules.js
+++ app/modules/users/user-validation-rules.js
@@ -1,6 +1,10 @@
-module.exports = {
+const rules = {
   'insert': container.joi.object({
     email: container.joi.string().required().email(),
     password: container.joi.string().required(),
   }),
+};
+
+module.exports = (method) => {
+  return container.validator.body(rules[method]);
 };
