00328f46621aeff53194188e0f69b86c0bf00a6e
Galoomba <david.hany129@gmail.com>
2019-07-07 02:36:19 +0200

    rolling diamond

--- app/config/knexfile.js
+++ app/config/knexfile.js
@@ -2,6 +2,7 @@ module.exports = {
   client: process.env.DB_DRIVER,
   connection: {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
