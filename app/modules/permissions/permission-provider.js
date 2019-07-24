63ba1498f056888591a751d59e66f844dd424b2f
Sherif Elfadaly <sh.elfadaly@gmail.com>
2019-07-24 10:05:48 +0200

    Remove groups & permissions

--- app/modules/permissions/permission-provider.js
+++
@@ -1,7 +0,0 @@
-module.exports = (container) => {
-  container.constant('permissionModel', require('./permission-model'));
-  container.service('permissionRepository', require('./permission-repository'), 'permissionModel');
-  container.service('permissionController', require('./permission-controller'), 'permissionRepository');
-  container.constant('permissionRoutes', require('./permission-routes'));
-  container.constant('permissionValidationRules', require('./permission-validation-rules'));
-};
