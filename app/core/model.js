1a7d46082823dd205f5d3db93cc403105dfffb11
Sherif Elfadaly <sh.elfadaly@gmail.com>
2019-10-24 11:37:51 +0200

    remove unnecessary code in base model

--- app/core/model.js
+++ app/core/model.js
@@ -15,10 +15,6 @@ class Model extends objection {
     // eslint-disable-next-line no-undef
     return Promise.resolve(super.$beforeInsert(context)).then(() => {
       this.created_at = container.moment().format('YYYY-MM-DD HH:mm:ss');
-      if (this.date) this.date = container.moment(this.date).format('YYYY-MM-DD HH:mm:ss');
-      if (this.start_date) this.start_date = container.moment(this.start_date).format('YYYY-MM-DD HH:mm:ss');
-      if (this.end_date) this.end_date = container.moment(this.end_date).format('YYYY-MM-DD HH:mm:ss');
-      if (this.hire_date) this.hire_date = container.moment(this.hire_date).format('YYYY-MM-DD HH:mm:ss');
     });
   }
 
@@ -35,10 +31,6 @@ class Model extends objection {
     return Promise.resolve(super.$beforeUpdate(queryOptions, context)).then(() => {
       this.updated_at = container.moment().format('YYYY-MM-DD HH:mm:ss');
       if (this.created_at) this.created_at = container.moment(this.created_at).format('YYYY-MM-DD HH:mm:ss');
-      if (this.date) this.date = container.moment(this.date).format('YYYY-MM-DD HH:mm:ss');
-      if (this.start_date) this.start_date = container.moment(this.start_date).format('YYYY-MM-DD HH:mm:ss');
-      if (this.end_date) this.end_date = container.moment(this.end_date).format('YYYY-MM-DD HH:mm:ss');
-      if (this.hire_date) this.hire_date = container.moment(this.hire_date).format('YYYY-MM-DD HH:mm:ss');
     });
   }
 }
