82aeb70effad0ce2b1091b03df685c9579505cac
Sherif Elfadaly <sh.elfadaly@gmail.com>
2019-12-09 15:20:14 +0200

    Add port to knex xonfig

--- app/config/app.js
+++ app/config/app.js
@@ -19,6 +19,7 @@ module.exports = {
   db_user: process.env.DB_USER || '',
   db_password: process.env.DB_PASSWORD || '',
   db_name: process.env.DB_NAME || '',
+  db_port: process.env.DB_PORT || '',
 
   /**
    * Auth config.
